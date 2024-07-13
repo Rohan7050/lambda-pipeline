bf01ca902705701286941f5dddd43d5e1df51d41
Rohan Pagare <rohanpagare7599@gmail.com>
2024-07-13 17:17:38 +0530

    chnage text

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify("change text!"),
+    body: JSON.stringify("this is testing change!"),
   };
   return response;
 };
